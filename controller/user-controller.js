c494e4a86253c61c217ef3e182c988229a763370
Muqthar Ahmed Quraishi J <muqthar401@gmail.com>
2022-02-01 23:56:03 +0530

    changes in user-controller and routes

--- controller/user-controller.js
+++ controller/user-controller.js
@@ -1,6 +1,63 @@
 import User from "../model/userSchema.js";
 
-const getUsers = async (request, response) => {
+export const registerUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await new User({
+    email,
+    password
+  });
+
+  if (email == "" || password == "") {
+    res.send("Email or password is empty");
+  }
+
+  const exist = await User.findOne({ email });
+
+  if (exist) {
+    res.json({ status: 200, message: "Email already exists" });
+  } else {
+    const createdUser = await user.save();
+    res.json({
+      status: 200,
+      message: "User created successfully",
+      data: createdUser
+    });
+  }
+};
+
+export const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  if (email == "" || password == "") {
+    res.json({ message: "Email or password is empty" });
+  }
+
+  if (email !== "email12@gmail.com") {
+    res.json({ message: "Email is required" });
+  }
+
+  const user = await User.findOne({ email, password });
+
+  if (user) {
+    res.json({
+      status: 200,
+      message: "User logged in successfully",
+      data: user
+    });
+  } else {
+    res.json({ status: 401, message: "Email not found" });
+  }
+};
+
+export const getUser = async (req, res) => {
+  const user = await User.find({});
+  if (user) res.json({ status: 200, message: "User found", data: user });
+  else {
+    res.json({ status: 401, message: "User not found" });
+  }
+};
+
+export const getUsersData = async (request, response) => {
   try {
     const users = await User.find({});
     response.json(users);
@@ -9,4 +66,40 @@ const getUsers = async (request, response) => {
   }
 };
 
-export default getUsers;
+export const getUserById = async (req, res) => {
+  const user = await User.findById(req.params.id);
+
+  if (user) {
+    res.json({ status: 200, message: "User found", data: user });
+  } else {
+    res.json({ status: 401, message: "User not found" });
+  }
+};
+
+export const updateUser = async (req, res) => {
+  const user = await User.findById(req.params.id);
+  if (user) {
+    (user.email = req.body.email || user.email),
+      (user.password = req.body.password || user.password);
+
+    const updateUser = await user.save();
+
+    res.status(200).json({
+      status: 200,
+      message: "User updated successfully",
+      data: updateUser
+    });
+  } else {
+    res.json({ status: 401, message: "User not updated" });
+  }
+};
+
+export const deleteUser = async (req, res) => {
+  const user = await User.findById(req.params.id);
+  if (user) {
+    await user.remove();
+    res.json({ status: 200, message: "User deleted successfully" });
+  } else {
+    res.json({ status: 401, message: "User not found" });
+  }
+};
--- routes/routes.js
+++ routes/routes.js
@@ -1,13 +1,31 @@
 import express from "express";
-import {getProducts,getProductById} from "../controller/product-controller.js";
-import getUsers from "../controller/user-controller.js";
+import {
+  getProducts,
+  getProductById
+} from "../controller/product-controller.js";
+import {
+  getUsersData,
+  registerUser,
+  loginUser,
+  getUser,
+  getUserById,
+  updateUser,
+  deleteUser
+} from "../controller/user-controller.js";
 
 const router = express.Router();
 
-router.get("/users", getUsers);
+//users
+router.post("/users/register", registerUser);
+router.get("/users/login", loginUser);
+router.get("/users/userData", getUsersData);
+router.get("/users/getUser", getUser);
+router.get("/users/getUserById", getUserById);
+router.put("/users/updateUser", updateUser);
+router.delete("/users/deleteUser", deleteUser);
 
+//products
 router.get("/products", getProducts);
-
-router.get("/product/:id",getProductById);
+router.get("/product/:id", getProductById);
 
 export default router;
